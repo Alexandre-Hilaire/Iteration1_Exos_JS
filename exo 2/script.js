b3cc00ad3ae1979078c68b6f1fa07fc9977b59b5
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-07-18 14:47:08 +0200

    Ajout de la version final de l'exo 2

--- exo 2/script.js
+++ exo 2/script.js
@@ -1,17 +1,28 @@
 // you can write js here
 console.log('exo-2');
 
-const myDate = new Date(Date.now());
+let isTesting  = true;
 
+    const myDate = new Date(Date.now()); // myDate est la date d'aujourd'hui
 
+console.log(myDate);
+//Les variables messages
 let week_day = "Nous sommes un jour de semaine";
 let week_end = "Nous sommes le week-end";
+//les variable de date (jour) et heure
+let day = myDate.getDay();
+let hour = myDate.getHours();
 
-const today = myDate.getDay();
-console.log(today);
+// si isTesting est vrai demande le jour et l'heure
+if (isTesting){
+    day = prompt('entrez la date test',"");
+    hour = prompt("entrez l'heure test","");
+}
 
-if (today > 0 && today < 6) {
+//Condition des messages
+if ((day == 1 && hour > 9) || (day == 5 && hour > 17) || (day > 1 && day < 5)) {
     console.log(week_day);
+    console.log(today, hour);
 }
 else {
     console.log(week_end);
