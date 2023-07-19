338034f0f22071e534a2811a603c6f9a300b198f
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-07-19 09:04:56 +0200

    Fin de l'exo 3 ajout de la fonction playGame et du cheat code

--- exo 3/exo3.js
+++ exo 3/exo3.js
@@ -1,11 +1,11 @@
 // you can write js here
 console.log('exo-3');
 
-let player_input = prompt("Your choice (rock, paper, scissors) ?)");
-player_input.toLowerCase();
+let player_input = prompt("Your choice (rock, paper, scissors) ?)"); // Demande au joueur de rentrer une réponse
+player_input = player_input.toLowerCase(); // Converti la chaîne de caractère en minuscules
 
-function get_player_choice (player_input) {
-    if (player_input === "rock" || player_input === "paper" || player_input === "scissors"){
+function get_player_choice (player_input) { // Si le chaîne de caractère est valide la fonction renvoie la réponse et l'affiche à la console
+    if (player_input === "rock" || player_input === "paper" || player_input === "scissors" || player_input === "bomb"){
         return player_input;
     }
     else {
@@ -15,8 +15,8 @@ function get_player_choice (player_input) {
 
 }
 
-function get_computer_choice(){
-    let computer = Math.floor(Math.random() * 3);
+function get_computer_choice(){ // La fonction demande 3 nombres aléatoire en 0 et 2 et selon le nombre renvoie une réponse
+    let computer = Math.floor(Math.random() * 3); //Math.floor pour abtenir des entiers
     if (computer == 0) {
         computer = 'rock';
     }
@@ -29,20 +29,34 @@ function get_computer_choice(){
     return computer
 }
 
-let computer = get_computer_choice();
+//let computer = get_computer_choice();
 
-console.log("choix de l'ordinnateur", computer);
 
-function find_winner(get_player_choice, get_computer_choice){
+
+//console.log("choix de l'ordinnateur", computer);
+
+function find_winner(get_player_choice, get_computer_choice){ // Comparaison des réponse 
     if(get_player_choice == get_computer_choice) {
         console.log("tied");
     }
     else if ((get_player_choice == "rock" && get_computer_choice == "paper") ||
              (get_player_choice == "scissors" && get_computer_choice == "rock") ||
-             (get_player_choice == "paper" && get_computer_choice == "scissors") ){
+             (get_player_choice == "paper" && get_computer_choice == "scissors")){
         console.log("L'ordinnateur à gagné");
+    }
+    else if (get_player_choice == "bomb") {
+        console.log("Vous avez gagné !");
     }
     else {
         console.log("Vous avez gagné");
     }
 }
+
+function play_game () { //Fonction qui fait fonctionner entièrement le jeu
+    u_choice = get_player_choice(player_input);
+    computer_choice = get_computer_choice();
+    console.log(u_choice);
+    console.log(computer_choice);
+    console.log(find_winner(u_choice, computer_choice));
+}
+play_game();
